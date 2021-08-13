ad1b91e36b770481390d959a281a7bea0f6a40a5
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-12 17:25:04 -0700

    added appt and character component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Router, Switch, useParams } from '../../state/RouterProvider';
+import CharacterList from '../characters/CharacterList';
+import CharacterToggle from '../characters/CharacterToggle';
+
+const Home = () => {
+  return <h1>Home page</h1>;
+};
+
+const About = () => {
+  const { name } = useParams();
+  return <h1>About page {name}</h1>;
+};
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <CharacterToggle />
+      <CharacterList />
+    </>
+  );
 }
